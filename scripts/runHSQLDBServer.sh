e610a745e29af1dfcb1067a00e0c1de81b01d11f
Kuba Neumann <kuba.neumann@gmail.com>
2012-01-28 15:25:49 +0100

    Eeasier way to run HSQLDB Server & Client

--- scripts/runHSQLDBServer.sh
+++ scripts/runHSQLDBServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp /opt/devel/db/hsqldb-2.2.4/lib/hsqldb.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp ./scripts/hsqldb-2.2.4.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
